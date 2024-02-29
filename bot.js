5cf71891459bf2dd2bb1c61746430bb8180c77bb
Cristian Martos Garcia <cristian.martos@dc.sothis-ti.com>
2024-02-29 13:44:02 +0100

    make it better

--- bot.js
+++ bot.js
@@ -7,8 +7,7 @@ bot.start((ctx) => {
 });
 
 bot.hears("Hola", ctx => {
-    ctx.reply("Hola " + ctx.from.first_name + " " + ctx.from.last_name);
-    ctx.reply("Como estas?", Markup.inlineKeyboard([
+    ctx.reply("Como estas?" + ctx.from.first_name + " " + ctx.from.last_name, Markup.inlineKeyboard([
         Markup.button.callback('Bien', 'bien'),
         Markup.button.callback('Regular', 'regular'),
         Markup.button.callback('Mal', 'mal')
